86b553a4b269ba93d518d9b83ce3b29b9ca0f212
RuMikuRu <43915493+RuMikuRu@users.noreply.github.com>
2020-11-14 15:13:01 +0300

    v1.1

--- Example11/src/main/java/com/mycompany/example11/List.java
+++ Example11/src/main/java/com/mycompany/example11/List.java
@@ -8,11 +8,39 @@ package com.mycompany.example11;
 /**
  *
  * @author Erishkegal
+ * @param <T>
  */
-public class List {
-    
-    public static void main(String []args)
-    {
-        System.out.print("Hello");
+public class List<T> {
+   public int getSize() {
+        int n=0;
+        for(Node<T>i = head; i!=null ;  i=i.next )n++;
+        return n;
     }
+   class Node<T>{
+        private T info;
+        private Node<T> next;
+
+        public Node(T info, Node<T> next) {
+            this.info = info;
+            this.next = next;
+        }
+
+        public Node() {
+        }
+
+        public Node(T info) {
+            this.info = info;
+        }
+
+        public void setInfo(T info) {
+            this.info = info;
+        }
+
+        public void setNext(Node<T> next) {
+            this.next = next;
+        } 
+    }
+    Node<T> head;
+    Node<T> tail;
+    
 }
---
+++ Example11/src/main/java/com/mycompany/example11/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.example11;
+import com.mycompany.example11.List;
+/**
+ *
+ * @author Erishkegal
+ */
+public class Main {
+     public static void main(String []args)
+    {
+        System.out.print("Hello");
+    }
+}
